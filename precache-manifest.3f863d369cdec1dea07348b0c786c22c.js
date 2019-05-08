faf1b0ba8fc9f711f8c431d665fb04fd09c97597
World Wide <agwalker249@gmail.com>
2019-05-07 19:42:06 -0500

    Updates

---
+++ precache-manifest.3f863d369cdec1dea07348b0c786c22c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616975fe01bee2cf7a1ffc4d8b69e0cc",
+    "url": "/Pupster/index.html"
+  },
+  {
+    "revision": "4043518915c97d985613",
+    "url": "/Pupster/static/css/main.bc2e7f1f.chunk.css"
+  },
+  {
+    "revision": "6dafae50ebf875fc793e",
+    "url": "/Pupster/static/js/2.44e3117d.chunk.js"
+  },
+  {
+    "revision": "4043518915c97d985613",
+    "url": "/Pupster/static/js/main.c267f572.chunk.js"
+  },
+  {
+    "revision": "6143040fb586cae5d558",
+    "url": "/Pupster/static/js/runtime~main.0b30e048.js"
+  }
+]);
